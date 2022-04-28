e60013c3ba9feacafe4e64b5728e0eb8b74cfb05
otomamatsashvili <otar.mamatsashvili@gmail.com>
2022-04-28 18:23:54 +0400

    feat: implement routing

--- src/App.js
+++ src/App.js
@@ -7,17 +7,28 @@ import {
   CovidPolitics,
   Thanks,
 } from './pages';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
-      <Start />
-      <Identification />
-      <CovidStatus />
-      <Vaccination />
-      <CovidPolitics />
-      <Thanks />
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Start />} />
+        <Route path='/questionnaire/1' element={<Identification />} />
+        <Route path='/questionnaire/2' element={<CovidStatus />} />
+        <Route path='/questionnaire/3' element={<Vaccination />} />
+        <Route path='/questionnaire/4' element={<CovidPolitics />} />
+        <Route path='/thanks' element={<Thanks />} />
+        <Route
+          path='*'
+          element={
+            <main className='flex justify-center items-center min-h-screen md:text-2xl'>
+              <p>არასწორ მისამართზე მოხვდი მეგობარო &#128579;</p>
+            </main>
+          }
+        />
+      </Routes>
+    </Router>
   );
 };
 
